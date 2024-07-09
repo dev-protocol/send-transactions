468ff692a389236939a975b61dcf0c220b55a382
Yash Agrawal <yash@devprotocol.xyz>
2024-07-09 12:53:53 +0530

    debug: add console.logs

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,6 +1,8 @@
+/* eslint-disable functional/no-expression-statements */
 const { API_KEY } = import.meta.env
 
 export const auth = (req: Request): boolean => {
 	const key = req.headers.get('authorization')?.replace(/Bearer(\s)+/i, '')
+	console.log('KEY', key)
 	return key === API_KEY
 }
